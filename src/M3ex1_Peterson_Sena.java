8d9282096f98313b27d4eaf877d1dd6d9bb0bf51
elpupas <petersonsena24@gmail.com>
2023-03-18 23:51:33 +0100

    primer commit

---
+++ src/M3ex1_Peterson_Sena.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/*
+ * ejercicio que pide al usuario, nombre y apellido y edad y luego lo imprime por pantalla
+ * ejercicie, that want ar user, name and lastname, age and late print for screen 
+ */
+
+public class M3ex1_Peterson_Sena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         String nom;
+         String cog;
+         int edad;
+        // full name with age, printscreen 
+         Scanner entrada = new Scanner(System.in);
+       System.out.println("Escriba su nombre");  
+       nom = entrada.nextLine();
+       System.out.println("Escriba sus apellidos");
+       cog = entrada.nextLine();
+       System.out.println("escriba su edad");
+       edad = entrada.nextInt();
+       
+       System.out.println("Su nombre es " + nom + " " + cog + " con " + edad 
+    		   + " años");
+       
+       
+         
+	}
+
+}
